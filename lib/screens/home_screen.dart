0f574d395ddaa434baa580a93f8b7e683ca3e084
DaviesNyantakyi <davies.nyantakyi@outlook.col>
2023-12-04 15:46:42 +0100

    Refactor folders

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_card.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _Avatar(),
+              Divider(),
+              _ContactInfo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class _Avatar extends StatelessWidget {
+  const _Avatar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SizedBox(
+          width: 100,
+          height: 100,
+          child: CircleAvatar(
+            backgroundImage: AssetImage(
+              'assets/20230815_180045 (1).jpg',
+            ),
+          ),
+        ),
+        Text(
+          'Davies Nyantakyi',
+          style: TextStyle(fontSize: 24, color: Colors.black),
+        ),
+        Text(
+          'Flutter Developer',
+          style: TextStyle(fontSize: 16, color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
+
+class _ContactInfo extends StatelessWidget {
+  const _ContactInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        MyCustomCard(
+          iconData: Icons.phone,
+          text: '+32 465252424',
+        ),
+        SizedBox(height: 8),
+        MyCustomCard(
+          iconData: Icons.phone,
+          text: 'davies.nyantakyi@outlook.com',
+        )
+      ],
+    );
+  }
+}
